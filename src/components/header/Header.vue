0248e5d216dfcec3b73370b6524226ae2a45bc85
nextc <1391040917@qq.com>
2025-05-04 23:23:33 +0800

    feat: 完善header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -5,13 +5,69 @@
     <div class="flex-1">
       <e-logo />
     </div>
+    <div class="flex gap-1 justify-center items-center">
+      <a-button
+        v-for="item in toolList"
+        shape="circle"
+        type="text"
+        class="text-[#1a79ff] [&_.border]:border-[#1a79ff] bg-sky-100/50 hover:bg-sky-100 hover:text-[#1a79ff] [&_.border]:hover:text-[#1a79ff]"
+        :key="item"
+        :icon="getIcon(item)"
+        @click="selectTool(item)"
+      />
+    </div>
+    <a-divider type="vertical" />
+    <div class="flex gap-1 justify-center items-center"></div>
   </div>
 </template>
 
 <script setup>
+import { h } from 'vue'
+import { icons } from 'lucide-vue-next'
+
 import ELogo from '@components/header/Logo'
+import Icon from '@components/Icon'
 
 defineOptions({
   name: 'EHeader',
 })
+
+const toolList = [
+  'Square',
+  'SquareFill',
+  'Circle',
+  'Slash',
+  'MoveDownLeft',
+  'Pencil',
+  'Magnifier',
+  'Step',
+  'Smile',
+]
+const getIcon = (item) => {
+  let icon = null
+  if (item.includes('Fill')) {
+    const type = item.replace('Fill', '')
+    const Icons = icons[type]
+    icon = h(Icons, { size: 16, fill: 'currentColor' })
+  } else if (item === 'Magnifier') {
+    icon = h(Icon.MessageCirclePlus, { size: 16 })
+  } else if (item === 'Step') {
+    icon = h(
+      'div',
+      {
+        key: item,
+        class:
+          'border text-xs border-black dark:border-white w-4 h-4 rounded-full text-center leading-4',
+      },
+      1
+    )
+  } else {
+    const Icons = icons[item]
+    icon = h(Icons, { name: item, size: 16 })
+  }
+  return icon
+}
+const selectTool = (item) => {
+  console.log(item)
+}
 </script>
