97f0f0b5e7aef66b651e0a1f8c1a6ca6b03cd91d
nextc <1391040917@qq.com>
2025-05-04 12:46:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <div>
+    <a-style-provider>
+      <a-config-provider
+        :theme="{
+          token: {
+            colorPrimary: '#00b96b',
+          },
+        }"
+      >
+        <div
+          id="shoteasy-container"
+          class="polka flex flex-col overflow-hidden antialiased w-full h-[100vh] dark:bg-black"
+        >
+          <div class="flex flex-col flex-1 h-0 md:flex-row md:items-stretch">
+            <a-button>按钮</a-button>
+          </div>
+        </div>
+      </a-config-provider>
+    </a-style-provider>
+  </div>
+</template>
