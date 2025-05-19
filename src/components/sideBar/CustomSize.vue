ee8b3e65d34b0bbd5288e2c431fefcf581cf17ec
nextc <1391040917@qq.com>
2025-05-19 22:46:28 +0800

    feat: 完善 SizeBar 组件

--- src/components/sideBar/CustomSize.vue
+++ src/components/sideBar/CustomSize.vue
@@ -1,3 +1,52 @@
-<template></template>
+<template>
+  <div class="flex gap-2 items-center py-2 font-normal">
+    <a-input-number
+      :min="1"
+      :value="width"
+      :placeholder="frameWidth"
+      class="flex-1"
+    >
+      <template #prefix>
+        <span class="opacity-60 mx-1">W</span>
+      </template>
+    </a-input-number>
+    <span class="text-xs opacity-50">x</span>
+    <a-input-number
+      :min="1"
+      :value="height"
+      :placeholder="frameHeight"
+      class="flex-1"
+    >
+      <template #prefix>
+        <span class="opacity-60 mx-1">H</span>
+      </template>
+    </a-input-number>
+    <a-button
+      type="primary"
+      shape="circle"
+      class="icon-btn"
+      :icon="h(Icon.Check, { size: 18 })"
+      :disabled="!width || !height"
+    ></a-button>
+    <a-tooltip title="Auto size">
+      <a-button
+        type="primary"
+        shape="circle"
+        class="icon-btn"
+        :icon="h(Icon.Maximize, { size: 18 })"
+        :disabled="type === 'auto'"
+      ></a-button>
+    </a-tooltip>
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import { ref, h } from 'vue'
+
+import Icon from '@components/Icon'
+
+const height = ref('')
+const width = ref('')
+const frameWidth = ref(1920)
+const frameHeight = ref(1080)
+</script>
--- src/components/sideBar/SizeBar.vue
+++ src/components/sideBar/SizeBar.vue
@@ -41,6 +41,43 @@
       <chevron-up v-if="open" :size="16" />
       <chevron-down v-else :size="16" />
     </div>
+    <template #title>
+      <custom-size :frameWidth="800" :frameHeight="600" />
+    </template>
+    <template #content>
+      <div
+        class="border-t border-gray-200 dark:border-gray-800 py-2 divide-y divide-gray-200 dark:divide-gray-700"
+        data-mode="light"
+      >
+        <div v-for="item in sizeConfig" :key="item.key">
+          <div v-if="item.key !== 'default'" class="font-semibold pt-2">
+            {{ item.title }}
+          </div>
+          <section class="flex flex-wrap items-end pb-2">
+            <a-button
+              v-for="(child, index) in item.lists"
+              :key="index"
+              type="text"
+              class="size-bar-btn flex-[33%] p-3 flex-col gap-0 disabled:bg-blue-500/5 disabled:border-blue-500 disabled:cursor-default disabled:text-black"
+              :disabled="checkSelected(item.key, child)"
+            >
+              <div class="py-2 px-3 w-full">
+                <div
+                  class="border border-black/50 bg-black/10 dark:bg-white/20 dark:border-white/40 w-full flex items-center justify-center rounded-md opacity-75"
+                  :style="{ aspectRatio: child.w / child.h }"
+                >
+                  <span>{{ child.w }} : {{ child.h }}</span>
+                </div>
+              </div>
+              <div v-if="child.title" class="text-xs">{{ child.title }}</div>
+              <div class="text-xs overflow-hidden text-gray-500">
+                {{ child.width }} x {{ child.height }}
+              </div>
+            </a-button>
+          </section>
+        </div>
+      </div>
+    </template>
   </a-popover>
 </template>
 
@@ -63,4 +100,8 @@ const handleOpenChange = (val) => {
 
 const height = ref(500)
 const isShowSize = ref(false)
+
+const checkSelected = (key, item) => {
+  return false
+}
 </script>
