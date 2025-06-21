0faa117cb5b42ed85464c093e2590bae7976d9cb
nextc <1391040917@qq.com>
2025-06-22 05:40:48 +0800

    feat：完善

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="md:w-0 md:flex-1 overflow-hidden select-none relative">
-    <div ref="target" class="w-full h-full relative z-0">
-      <e-view v-if="showView" :target="target" />
-    </div>
-    <e-zoom />
+    <EView />
+    <EZoom />
   </div>
 </template>
 
 <script setup>
-import { useTemplateRef, computed } from 'vue'
 import EView from './View'
 import EZoom from './Zoom'
 defineOptions({
   name: 'EEditor',
 })
-
-const target = useTemplateRef('target')
-const showView = computed(() => target.value)
 </script>
--- src/components/editor/View.vue
+++ src/components/editor/View.vue
@@ -1,31 +1,41 @@
 <template>
-  <FrameBox
-    v-if="hasAppTree"
-    :parent="editorStore.app.tree"
-    :cursor="editorStore.cursor"
-    v-bind="optionStore.frameConf"
-  >
-    <template v-if="hasImgSrc" v-slot="{ frame }">
-      <ShapeLine
-        v-for="item in editorStore.shapesList"
-        :key="item.id"
-        :parent="frame"
-        v-bind="
-          Object.assign(
-            {},
-            item,
-            item.type === 'Magnifier' ? { snap: editorStore.snap } : {}
-          )
-        "
-      />
-      <Screenshot :parent="frame" />
-      <Watermark v-if="optionStore.waterImg" :parent="frame" />
-    </template>
-  </FrameBox>
+  <div class="w-full h-full relative z-0" ref="targetRef">
+    <FrameBox
+      v-if="hasAppTree"
+      :parent="editorStore.app.tree"
+      :cursor="editorStore.cursor"
+      v-bind="optionStore.frameConf"
+    >
+      <template v-if="hasImgSrc" v-slot="{ frame }">
+        <ShapeLine
+          v-for="item in editorStore.shapesList"
+          :key="item.id"
+          :parent="frame"
+          v-bind="
+            Object.assign(
+              {},
+              item,
+              item.type === 'Magnifier' ? { snap: editorStore.snap } : {}
+            )
+          "
+        />
+        <Screenshot :parent="frame" />
+        <Watermark v-if="optionStore.waterImg" :parent="frame" />
+      </template>
+    </FrameBox>
+  </div>
 </template>
 
 <script setup>
-import { watch, onUnmounted, computed, nextTick } from 'vue'
+import {
+  watch,
+  onUnmounted,
+  computed,
+  nextTick,
+  ref,
+  onMounted,
+  unref,
+} from 'vue'
 import { debounce } from 'lodash-es'
 import { addListener, removeListener } from 'resize-detector'
 import {
@@ -61,19 +71,13 @@ defineOptions({
 const editorStore = stores.useEditorStore()
 const optionStore = stores.useOptionStore()
 useHotKeys(stores)
-let timer = null
 
-const props = defineProps({
-  target: {
-    type: Object,
-    default: () => {},
-  },
-})
+const targetRef = ref(null)
 
 Cursor.set('pencil', { url: pencilPng })
 // 监听容器变化
 const setZoom = () => {
-  const { width, height } = props.target.getBoundingClientRect()
+  const { width, height } = targetRef.value.getBoundingClientRect()
   editorStore.app.tree.zoom('fit', 100)
   if (
     optionStore.frameConf.width < width &&
@@ -85,147 +89,6 @@ const setZoom = () => {
 const onResize = debounce(() => {
   setZoom()
 }, 50)
-
-watch(
-  () => props.target,
-  (target) => {
-    console.log(props.target, 'target')
-    if (target) {
-      const app = new App({
-        view: target,
-        editor: {
-          lockRatio: 'corner',
-          stroke: '#3f99f7',
-          skewable: false,
-          hover: false,
-          middlePoint: { cornerRadius: 100, width: 20, height: 6 },
-          rotatePoint: {
-            width: 20,
-            height: 20,
-            fill: {
-              type: 'image',
-              url: rotatePng,
-            },
-          },
-        },
-        tree: {
-          usePartRender: true,
-        },
-        sky: {
-          type: 'draw',
-          usePartRender: true,
-        },
-      })
-      new ScrollBar(app)
-      editorStore.setApp(app)
-      editorStore.app.tree.on(ZoomEvent.ZOOM, () => {
-        editorStore.setScale(editorStore.app.tree.scale)
-      })
-      editorStore.app.tree.on(ResizeEvent.RESIZE, () => {
-        editorStore.setScale(editorStore.app.tree.scale)
-      })
-      editorStore.app.editor.on(EditorMoveEvent.SELECT, (event) => {
-        const { list } = event
-        if (list.length < 2) return
-        if (list.some((e) => e.tag === 'Magnifier')) {
-          editorStore.app.editor.config.rotateable = false
-          editorStore.app.editor.config.lockRatio = true
-        } else {
-          editorStore.app.editor.config.rotateable = true
-          editorStore.app.editor.config.lockRatio = false
-        }
-      })
-
-      let shapeId = null
-      const onStart = (arg) => {
-        if (!editorStore.useTool) return
-        const { target } = arg
-        const shape = editorStore.getShape(target.id)
-        if (shape) return
-        shapeId = nanoid()
-        const size = arg.getPageBounds ? arg.getPageBounds() : arg.getPage()
-        const type = editorStore.useTool
-        const newShape = {
-          id: shapeId,
-          type,
-          fill: editorStore.annotateColor,
-          strokeWidth: editorStore.strokeWidth,
-          zIndex: editorStore.shapes.size + 1,
-          ...size,
-        }
-        return newShape
-      }
-      editorStore.app.tree.on(PointerEvent.DOWN, (arg) => {
-        const type = editorStore.useTool
-        if (type !== 'Step') return
-        const newShape = onStart(arg)
-        if (!newShape) return
-        newShape.text = editorStore.nextStep
-        newShape.editable = true
-        editorStore.addShape(newShape)
-        shapeId = null
-        editorStore.setUseTool(null)
-      })
-      editorStore.app.tree.on(DragEvent.START, (arg) => {
-        const type = editorStore.useTool
-        if (type === 'Step') return
-        const newShape = onStart(arg)
-        if (!newShape) return
-        if (['Slash', 'MoveDownLeft', 'Pencil'].includes(type)) {
-          newShape.points = [newShape.x, newShape.y]
-        }
-        editorStore.addShape(newShape)
-      })
-      editorStore.app.tree.on(DragEvent.DRAG, (arg) => {
-        if (!editorStore.useTool) return
-        if (!shapeId) return
-        const shape = editorStore.getShape(shapeId)
-        if (!shape) return
-        const size = arg.getPageBounds()
-        const max = Math.max(size.width, size.height)
-        if (shape.type === 'Magnifier') {
-          size.width = max
-          size.height = max
-        }
-        const newShape = Object.assign({}, shape, size)
-        const { points, type } = newShape
-        if (points && points.length) {
-          const { x, y } = arg.getInnerTotal()
-          const newX = x > 0 ? size.x + x : size.x
-          const newY = y > 0 ? size.y + y : size.y
-          if (type === 'Pencil') {
-            newShape.points = [...points, newX, newY]
-          } else {
-            newShape.points = [points[0], points[1], newX, newY]
-          }
-        }
-        editorStore.addShape(newShape)
-      })
-      editorStore.app.tree.on(DragEvent.END, () => {
-        if (!editorStore.useTool) return
-        if (!shapeId) return
-        const shape = editorStore.getShape(shapeId)
-        if (shape) {
-          if (
-            (shape.width === 0 || shape.height === 0) &&
-            !['Slash', 'MoveDownLeft', 'Pencil'].includes(shape.type)
-          ) {
-            editorStore.removeShape(shape)
-          } else {
-            editorStore.addShape(Object.assign({}, shape, { editable: true }))
-          }
-        }
-        shapeId = null
-        if (editorStore.useTool !== 'Pencil') editorStore.setUseTool(null)
-      })
-    }
-
-    addListener(target, onResize)
-  },
-  {
-    immediate: true,
-  }
-)
 
 watch(
   [() => optionStore.frameConf.width, () => optionStore.frameConf.height],
@@ -247,8 +110,143 @@ const hasImgSrc = computed(() => {
   return !!editorStore.img?.src
 })
 
+onMounted(() => {
+  const target = unref(targetRef)
+  if (target) {
+    addListener(target, onResize)
+
+    const app = new App({
+      view: target,
+      editor: {
+        lockRatio: 'corner',
+        stroke: '#3f99f7',
+        skewable: false,
+        hover: false,
+        middlePoint: { cornerRadius: 100, width: 20, height: 6 },
+        rotatePoint: {
+          width: 20,
+          height: 20,
+          fill: {
+            type: 'image',
+            url: rotatePng,
+          },
+        },
+      },
+      tree: {
+        usePartRender: true,
+      },
+      sky: {
+        type: 'draw',
+        usePartRender: true,
+      },
+    })
+    new ScrollBar(app)
+    editorStore.setApp(app)
+    app.tree.on(ZoomEvent.ZOOM, () => {
+      editorStore.setScale(editorStore.app.tree.scale)
+    })
+    app.tree.on(ResizeEvent.RESIZE, () => {
+      editorStore.setScale(editorStore.app.tree.scale)
+    })
+    app.editor.on(EditorMoveEvent.SELECT, (event) => {
+      const { list } = event
+      if (list.length < 2) return
+      if (list.some((e) => e.tag === 'Magnifier')) {
+        editorStore.app.editor.config.rotateable = false
+        editorStore.app.editor.config.lockRatio = true
+      } else {
+        editorStore.app.editor.config.rotateable = true
+        editorStore.app.editor.config.lockRatio = false
+      }
+    })
+
+    let shapeId = null
+    const onStart = (arg) => {
+      if (!editorStore.useTool) return
+      const { target } = arg
+      const shape = editorStore.getShape(target.id)
+      if (shape) return
+      shapeId = nanoid()
+      const size = arg.getPageBounds ? arg.getPageBounds() : arg.getPage()
+      const type = editorStore.useTool
+      const newShape = {
+        id: shapeId,
+        type,
+        fill: editorStore.annotateColor,
+        strokeWidth: editorStore.strokeWidth,
+        zIndex: editorStore.shapes.size + 1,
+        ...size,
+      }
+      return newShape
+    }
+    app.tree.on(PointerEvent.DOWN, (arg) => {
+      const type = editorStore.useTool
+      if (type !== 'Step') return
+      const newShape = onStart(arg)
+      if (!newShape) return
+      newShape.text = editorStore.nextStep
+      newShape.editable = true
+      editorStore.addShape(newShape)
+      shapeId = null
+      editorStore.setUseTool(null)
+    })
+    app.tree.on(DragEvent.START, (arg) => {
+      const type = editorStore.useTool
+      if (type === 'Step') return
+      const newShape = onStart(arg)
+      if (!newShape) return
+      if (['Slash', 'MoveDownLeft', 'Pencil'].includes(type)) {
+        newShape.points = [newShape.x, newShape.y]
+      }
+      editorStore.addShape(newShape)
+    })
+    app.tree.on(DragEvent.DRAG, (arg) => {
+      if (!editorStore.useTool) return
+      if (!shapeId) return
+      const shape = editorStore.getShape(shapeId)
+      if (!shape) return
+      const size = arg.getPageBounds()
+      const max = Math.max(size.width, size.height)
+      if (shape.type === 'Magnifier') {
+        size.width = max
+        size.height = max
+      }
+      const newShape = Object.assign({}, shape, size)
+      const { points, type } = newShape
+      if (points && points.length) {
+        const { x, y } = arg.getInnerTotal()
+        const newX = x > 0 ? size.x + x : size.x
+        const newY = y > 0 ? size.y + y : size.y
+        if (type === 'Pencil') {
+          newShape.points = [...points, newX, newY]
+        } else {
+          newShape.points = [points[0], points[1], newX, newY]
+        }
+      }
+      editorStore.addShape(newShape)
+    })
+    app.tree.on(DragEvent.END, () => {
+      if (!editorStore.useTool) return
+      if (!shapeId) return
+      const shape = editorStore.getShape(shapeId)
+      if (shape) {
+        if (
+          (shape.width === 0 || shape.height === 0) &&
+          !['Slash', 'MoveDownLeft', 'Pencil'].includes(shape.type)
+        ) {
+          editorStore.removeShape(shape)
+        } else {
+          editorStore.addShape(Object.assign({}, shape, { editable: true }))
+        }
+      }
+      shapeId = null
+      if (editorStore.useTool !== 'Pencil') editorStore.setUseTool(null)
+    })
+  }
+})
+
 onUnmounted(() => {
-  removeListener(props.target, onResize)
+  removeListener(targetRef.value, onResize)
   editorStore.destroy()
 })
 </script>
